3a41b8022819051ad2dc34f02f2c60c0a3dbdf01
Gustavo Romero <gustavo@43t.com.br>
2020-02-20 22:56:47 -0300

    ...

---
+++ js/partials.js
@@ -0,0 +1,18 @@
+$(document).ready(function($) {
+
+  loadMenu();
+});
+
+
+function loadMenu() {
+  console.log('Loading Menu!')
+
+  // $('#header-navbar').load('http://partials/header-navbar.html')
+
+  $.get("partials/header-navbar.html")
+  .done((data) => {
+      $('#header-navbar').html(data) ;
+  });
+
+}
+
