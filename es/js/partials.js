1ea4de85d399137bbd2d7b9e085c8050449698cf
Gustavo Romero <gustavo@43t.com.br>
2020-02-21 01:05:48 -0300

    ...

---
+++ es/js/partials.js
@@ -0,0 +1,16 @@
+
+document.addEventListener("DOMContentLoaded", loadPartials);
+
+function loadPartials() {
+
+
+  $('#topbar-content').load('partials/topbar-content.html')
+
+  $('#header-navbar').load('partials/header-navbar.html')
+
+  $('#footer-content').load('partials/footer-content.html')
+
+
+
+}
+
